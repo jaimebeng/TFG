46a7e4283b3de4474dae8dcb1a5821d647cdf0f5
Jaime Bengoechea <bengoecheapardo.jaime@gmail.com>
2026-02-20 19:07:01 +0100

    Fixing cleaning data

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -1,14 +1,22 @@
 import pandas as pd
+import os
 
 class CleanData():
     """Minimal data cleaner for already well-structured input files.
-    Applies only lightweight column/index formatting adjustments."""
+    Applies only lightweight column/index formatting adjustments.
+    Saves in data/clean.
+    """
 
     def __innit__(self,dfs):
         self.dfs = dfs
+        self.output_path = "/Users/jaime/Documents/UPM/TFG/data/clean"
+        os.makedirs(self.output_path, exist_ok=True)
     
     def clean_data(self):
 
-        for df in self.dfs:
+        for ticker,df in self.dfs.items():
             df = df.droplevel(1,axis=1)
             df.columns.name = None
+            file_path = os.path.join(self.output_path, f"{ticker}.csv")
+            df.to_csv(file_path,index=True) 
+
