a8b9eff1af66cd4edf3fa97ec59ae96fe0b677ad
Jaime Bengoechea <bengoecheapardo.jaime@gmail.com>
2026-02-27 11:17:24 +0100

    Adding main pipeline

---
+++ main.py
@@ -0,0 +1,25 @@
+from src.data.download_data import DownloadData
+from src.data.clean_data import CleanData
+
+
+DOWNLOAD = False
+CLEAN = False
+tickers = ["AAPL", "MSFT", "NVDA", "GOOG", "META", "AVGO","AMZN", "HD", "MCD", 
+           "NKE","JNJ", "UNH", "PFE", "ABBV","CAT", "BA", "UPS", "MMM","XOM", 
+           "CVX", "SLB","JPM", "BAC", "GS", "MS","NEE", "LIN", "DOW","VZ", "CMCSA"
+]
+
+def main():
+    if DOWNLOAD:
+        dd = DownloadData()
+        dd.download_tick_data(tickers)
+        dd.download_gspc()
+        print("Data downloaded successfully.")
+    if CLEAN:
+        cd = CleanData()
+        cd.clean_data()
+        print("Data cleaned successfully.")
+
+
+if __name__ == "__main__":
+    main()
