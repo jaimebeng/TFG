3122c351da7e7d01ec8fb81d044ca1a3b53ba19f
Jaime Bengoechea <bengoecheapardo.jaime@gmail.com>
2026-02-27 18:27:49 +0100

    Fixing data_loader

--- src/utils/data_loader.py
+++ src/utils/data_loader.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from pathlib import Path
 
 class DataLoader():
     """Utility class for loading project datasets from disk.
@@ -18,17 +19,19 @@ class DataLoader():
 
         return df
     
-    def load_multiple_data(self,type,tickers):
+    def load_multiple_data(self,type):
         if type not in ["raw","clean","processed","features"]:
             raise ValueError("Type must be raw, clean, processed or features")
-        
+
+        directory = os.path.join(self.data_path, type)
         dfs = {}
-        for ticker in tickers:
-            path = os.path.join(self.data_path, type, f"{ticker}.csv")
-            df = pd.read_csv(path, header=0, index_col=0)
+        for filename in os.listdir(directory):
+            full_path = os.path.join(directory, filename)
+            df = pd.read_csv(full_path, header=0, index_col=0)
+            file = Path(filename)
+            ticker = file.stem
             dfs[ticker] = df
             print(f"{ticker}.csv loaded succesfully")
-
-
+            
         return dfs
 
