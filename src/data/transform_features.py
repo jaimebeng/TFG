7c54e535a2a099f3c12dc3b963359e5163a7a0a1
Jaime Bengoechea <bengoecheapardo.jaime@gmail.com>
2026-04-14 16:35:18 +0200

    Reworking saving of transformed features to ensure alignment with target

--- src/data/transform_features.py
+++ src/data/transform_features.py
@@ -31,6 +31,7 @@ class FeatureTransformation():
         dfs = dl.load_multiple_data("features")
         df = self._dataset_joiner(dfs)
         X = df.drop(columns="Target")
+        y = df["Target"].reset_index(drop=True)
         months = np.sort(X.index.unique())
         X_cache = []
         transformer = PerStockTransformer()
@@ -39,8 +40,9 @@ class FeatureTransformation():
             X_slice = X[X.index <= month].reset_index(drop=True)
             Xt = trans.fit_transform(X_slice)
             X_cache.append(Xt)
+        dump(X_cache, os.path.join(self._output_path, "X_cache.joblib"))
+        dump(y, os.path.join(self._output_path, "y_aligned.joblib"))
+        dump(months, os.path.join(self._output_path, "months.joblib"))
 
-        cache_path = os.path.join(self._output_path, "X_cache.joblib")
-        dump(X_cache, cache_path)
         
 
