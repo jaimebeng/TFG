Fixing source for risk free rate, risk free rate calculation and monthly sampling of data to use NYSE dates

src/data/create_features.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 from pathlib import Path
+import pandas_market_calendars as mcal
 
 
 class FeatureCreation():
@@ -17,25 +18,32 @@ class FeatureCreation():
     def _features_engineering(self,df,ticker):
         df["Ticker"] = ticker
         df.dropna(how='any', inplace=True)
-        df = df.resample("BME").last()
+        nyse = mcal.get_calendar('NYSE')
+        nyse_bme = pd.offsets.CustomBusinessMonthEnd(calendar=nyse.regular_holidays)
+        df = df.resample(nyse_bme).last()
         df["Target"] = (np.log(df["Close"] / df["Close"].shift(1))).shift(-1)
         df.dropna(subset=["Target"], inplace=True)
         features = [
-            'High', 'Low', 'Open', 'Returns', 'MA60',
-            'Close', 'Normalized Average True Range', 'Percentage Intraday Range', 'Daily Range', 'True Range',
-            'Monthly Average True Range', 'Monthly Average Intraday Range', 'Quarterly Volatility', 'Monthly Average Candle Body', 'Average Gains',
-            'Average Losses', '14-day Average True Range', 'VMA20', 'Volume', 'RS',
-            "Zscore Price", "Mean Reversion Pressure", "Monthly Parkinson Volatility", "Monthly Rogers-Satchell Volatility", "Entropy of Returns",
-            "Cumulative Volume Trend", "Volatility Breakout", "Monthly Normalized Intraday Intensity", "Zscore Volume", "MA20 Alpha",
-            "Quarterly Alpha", "Semi-annual Alpha", "126 Day Beta", "Semi-annual Volatility", "Quarterly Alpha Volatility",
-            "Distance from MA60", "Daily Candle Body", "Monthly Volatility", "Upside Volatility", "Monthly Average Percentage Intraday Range",
-            "RSI", "Downside Volatility", "Monthly Log Returns", "Quarterly Log Returns", "Volatility Ratio",
-            "Log Returns", "MA20/MA60", "Losses", "Volatility Term Structure Slope", "Return Autocorr 10D",
-            "Trend Strength", "Monthly Return Momentum", "Monthly Kurtosis", "Gap Up", "Up Days Ratio",
-            "Gap vs Intraday Move", "Volume Weighted Return", "Price Volume Correlation", "Price Difference", "Quarterly Hurst Exponent",
-            "Volatility Regime Change", "Quarterly Efficiency Ratio", "21 Day Beta", "Signed Volume Pressure", "Monthly Close-Location Value",
-            "Daily Alpha", "K_Ratio_21d", "Volume_Imbalance", "VPIN_Signal_21d"
+            'High', 'Low', 'Open', 'Returns',
+            'MA20', 'Close', 'Monthly Average Percentage Intraday Range', 'True Range',
+            'Daily Range', 'Monthly Average Intraday Range', '14-day Average True Range', 'Percentage Intraday Range',
+            'Quarterly Volatility', 'Monthly Average Candle Body', 'Average Gains', 'Average Losses',
+            'MA60', 'VMA20', 'Volume', 'RS',
+            'Monthly Log Returns', 'Zscore Price', 'High Low Trend Position', 'Volatility Breakout',
+            'Monthly Parkinson Volatility', 'Monthly Volatility', 'Monthly Normalized Intraday Intensity', 'MA20 Alpha',
+            'Quarterly Alpha', 'Quarterly Log Returns', 'Monthly Return Momentum', 'Semi-annual Alpha',
+            '126 Day Beta', 'Semi-annual Volatility', 'Quarterly Alpha Volatility',
+            "Distance from MA60", "Daily Candle Body", "Normalized Average True Range", "Upside Volatility", 
+            "RSI", "Downside Volatility", "63 Day Beta", "Log Returns",
+            "Price Difference", "Distance from MA20", "Volume Flow Ratio", "Gains",
+            "Losses", "Body Up", "Monthly Efficiency Ratio", "Volatility Term Structure Slope",
+            "Volatility Regime Change", "Monthly Skewness", "Quarterly Efficiency Ratio", "Return Autocorr 5D",
+            "Return Autocorr 10D", "Gap Direction", "Monthly Close-Location Value", "Gap Persistence",
+            "Volume Weighted Return", "Signed Volume Pressure", "Dollar Volume", "21 Day Beta",
+            "Mean Reversion Pressure", "Up Days Ratio", "Daily Alpha",
+            "Amihud_Illiquidity", "K_Ratio_21d", "VPIN_Proxy"
         ]
+        
         df = df.drop(columns=features)
 
         df = df.copy()
@@ -46,7 +54,7 @@ class FeatureCreation():
     def create_features(self):
         directory = "/home/jaime/Documents/TFG/data/processed"
         for filename in os.listdir(directory):
-            if filename not in ["GSPC.csv","market_caps.csv","IRX.csv"]:
+            if filename not in ["GSPC.csv","market_caps.csv","GS3M.csv"]:
                 full_path = os.path.join(directory, filename)
                 df = pd.read_csv(full_path, header=0, index_col=0, parse_dates=True)
                 file = Path(filename)